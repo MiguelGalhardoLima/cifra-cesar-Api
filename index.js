5c498e58c5800a9be6a42251a5be31de11898fd1
MiguelGalhardoLima <miguelgalhardo76@gmail.com>
2025-09-09 19:16:23 -0300

    Primeiro commit

--- index.js
+++ index.js
@@ -9,7 +9,24 @@ app.listen(3000, () => {
 console.log("servidor rodando na porta: 3000");
 })
 
-const { criptografar, descriptografar } = require("./cesar");
+function criptografar(texto, chave){
+    chave = chave % 26;
+    let resultado= "";
+    for (let char of texto){
+          if (char.match(/[a-z]/)) {
+      resultado += String.fromCharCode((char.charCodeAt(0) - 97 + chave + 26) % 26 + 97);
+    } else if (char.match(/[A-Z]/)) {
+      resultado += String.fromCharCode((char.charCodeAt(0) - 65 + chave + 26) % 26 + 65);
+    } else {
+      resultado += char;
+    }
+  }
+  return resultado;
+}
+function descriptografar(texto, chave) {
+  return criptografar(texto, -chave);
+}
+
 
 app.post("/api/criptografar", (req, res) => {//Endpoint Criptografia
   const { texto, chave } = req.body;
